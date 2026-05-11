2abcfc8393d182176bab92daaac1310197fb67f5
itsdaxen <fa.rzam.sadegh@gmail.com>
2026-05-11 22:20:53 +0300

    Add tokenizer encoding and decoding with tests

--- src/llm_internals/tokenization/simple_tokenizer.py
+++ src/llm_internals/tokenization/simple_tokenizer.py
@@ -1,8 +1,27 @@
 import re
 
 TOKEN_SPLIT_PATTERN = r'([,.:;?_!"()\']|--|\s)'
+PUNCTUATION_SPACING_PATTERN = r'\s+([,.:;?!"()\'])'
+
 
 def tokenize(raw_text: str) -> list[str]:
     """Split text into tokens."""
     preprocessed = re.split(TOKEN_SPLIT_PATTERN, raw_text)
     return [item.strip() for item in preprocessed if item.strip()]
+
+
+class SimpleTokenizerV1:
+    """A simple tokenizer that can encode and decode (convert text to IDs and vice versa)."""
+
+    def __init__(self, vocab: dict[str, int]):
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text: str) -> list[int]:
+        ids = [self.str_to_int[token] for token in tokenize(text)]
+        return ids
+
+    def decode(self, ids: list[int]) -> str:
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(PUNCTUATION_SPACING_PATTERN, r"\1", text)
+        return text
--- tests/test_simple_tokenizer.py
+++ tests/test_simple_tokenizer.py
@@ -1,31 +1,48 @@
-from llm_internals.tokenization.simple_tokenizer import tokenize
+from llm_internals.tokenization.simple_tokenizer import SimpleTokenizerV1, tokenize
 
-def test_tokenization():
 
+def test_tokenize_splits_words_and_punctuation():
     text = "Hello, world. Is this-- a test?"
 
     tokens = tokenize(text)
 
     assert tokens == [
-
         "Hello",
-
         ",",
-
         "world",
-
         ".",
-
         "Is",
-
         "this",
-
         "--",
-
         "a",
-
         "test",
-
         "?",
+    ]
 
-    ] 
+
+def test_tokenizer_encodes_text_to_ids():
+    vocab = {
+        "Hello": 0,
+        ",": 1,
+        "world": 2,
+        ".": 3,
+    }
+    tokenizer = SimpleTokenizerV1(vocab)
+
+    ids = tokenizer.encode("Hello, world.")
+
+    assert ids == [0, 1, 2, 3]
+
+
+def test_tokenizer_decodes_ids_to_text():
+    vocab = {
+        "Hello": 0,
+        ",": 1,
+        "world": 2,
+        ".": 3,
+    }
+    tokenizer = SimpleTokenizerV1(vocab)
+
+    text = tokenizer.decode([0, 1, 2, 3])
+
+    assert text == "Hello, world."
